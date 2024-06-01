65180c650161d2afeccd725ab13f15713db2a598
Abiola Toluwanimi Esther <toluwanimi97@gmail.com>
2024-06-01 07:32:20 +0100

    feat:future clan impact component (#1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Impact from './Impact.js'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Impact/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Impact.js
@@ -0,0 +1,19 @@
+import image from './Frame 19.jpg'
+import './impact.css'
+function Impact(){
+    return(
+        <div class='main'>
+            <div class='struct2'>
+                <h1>Deliberate And <br></br> Strategic Impact</h1>
+                <p>As a beacon of innovation and learning, <br></br>
+                   we are dedicated to altering lives and  <br></br>
+                   communities</p>
+            </div>
+            <div class='struct3'>
+             <img id='team' width={'100%'} src={image} alt='team work'/>
+            </div>
+        </div>
+        
+    )
+}
+export default Impact
